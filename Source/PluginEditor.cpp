c247effa934d9ea3a4afd50a86a06e7ee4c5eca7
aerdni99 <adi19@uakron.edu>
2023-04-05 02:44:17 -0400

    feat: add arpeggiator component to editor

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,11 +13,12 @@
 MySynthAudioProcessorEditor::MySynthAudioProcessorEditor (MySynthAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p), adsr(audioProcessor.apvts), osc1(audioProcessor.apvts, "OSC1", "OSC1GAIN", "OSC1PITCH"), osc2(audioProcessor.apvts, "OSC2", "OSC2GAIN", "OSC2PITCH"), arp(audioProcessor.apvts)
 {
-    setSize (600, 300);
+    setSize (800, 300);
 
     addAndMakeVisible(osc1);
     addAndMakeVisible(osc2);
     addAndMakeVisible(adsr);
+    addAndMakeVisible(arp);
 }
 
 MySynthAudioProcessorEditor::~MySynthAudioProcessorEditor()
@@ -32,7 +33,8 @@ void MySynthAudioProcessorEditor::paint (juce::Graphics& g)
 
 void MySynthAudioProcessorEditor::resized()
 {
-    adsr.setBounds(10, getHeight() / 2 + 5, getWidth() - 20, getHeight() / 2 - 15);
-    osc1.setBounds(10, 10, getWidth() / 2 - 15, getHeight() / 2 - 15);
-    osc2.setBounds(getWidth() / 2 + 5, 10, getWidth() / 2 - 15, getHeight() / 2 - 15);
+    adsr.setBounds(10, getHeight() / 2 + 5, getWidth() - 220, getHeight() / 2 - 15);
+    osc1.setBounds(10, 10, (getWidth() - 200) / 2 - 15, getHeight() / 2 - 15);
+    osc2.setBounds((getWidth() - 200) / 2 + 5, 10, (getWidth() - 200) / 2 - 15, getHeight() / 2 - 15);
+    arp.setBounds(605, 10, 180, getHeight() - 20);
 }
